d9e27e2efc2cd9a540c5a27512f3cf90c4edfb7e
sforerol <ser18fl@gmail.com>
2020-12-05 14:42:55 -0500

    filterin data

--- getDividends/script.R
+++ getDividends/script.R
@@ -4,4 +4,5 @@ url <- "https://finance.yahoo.com/quote/SPSM/history?period1=1448668800&period2=
 h <- read_html(url)
 tab <- h %>% html_nodes("table")
 table <- tab %>% html_table(fill = TRUE)
-
+tabledf <- data.frame(table)
+tabledf <- tabledf[1:21,1:2]
