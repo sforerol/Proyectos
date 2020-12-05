be6c9f88ed91c78d8d564484909c60dcbba69354
sforerol <ser18fl@gmail.com>
2020-12-05 15:12:20 -0500

    Finishes defining table there is error on wrangling

--- getDividends/script.R
+++ getDividends/script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rvest)
+
 url <- "https://finance.yahoo.com/quote/SPSM/history?period1=1448668800&period2=1606521600&interval=div%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true"
 h <- read_html(url)
 tab <- h %>% html_nodes("table")
@@ -9,3 +10,7 @@ tabledf <- tabledf[1:21,1:2]
 dates <- tabledf[,1]
 dividends <-gsub(" .*$","",tabledf[,2])
 dividends <- as.numeric(dividends)
+exportTable <- merge(dates,dividends)
+names(exportTable)[1] <- paste("Dates")
+names(exportTable)[2] <- paste("Dividends")
+
