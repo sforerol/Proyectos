2bb0784d9985269132e99e0990d99981caf148ea
sforerol <ser18fl@gmail.com>
2020-12-05 14:18:20 -0500

    Downloading data from yahoo finance

--- getDividends/script.R
+++ getDividends/script.R
@@ -1,4 +1,7 @@
 library(tidyverse)
 library(rvest)
 url <- "https://finance.yahoo.com/quote/SPSM/history?period1=1448668800&period2=1606521600&interval=div%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true"
+h <- read_html(url)
+tab <- h %>% html_nodes("table")
+table <- tab %>% html_table(fill = TRUE)
 
