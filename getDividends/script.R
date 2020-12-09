5a5a6cae7ef8ead3c50d66c4334029fc3e94f7c7
sforerol <ser18fl@gmail.com>
2020-12-08 20:13:37 -0500

    Ready for testing after fixing indexes

--- getDividends/script.R
+++ getDividends/script.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(rvest)
-
+library("writexl")
 url <- "https://finance.yahoo.com/quote/SPSM/history?period1=1448668800&period2=1606521600&interval=div%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true"
 h <- read_html(url)
 tab <- h %>% html_nodes("table")
@@ -11,5 +11,5 @@ dates <- tabledf[,1]
 dividends <-gsub(" .*$","",tabledf[,2])
 dividends <- as.numeric(dividends)
 exportTable <- data.frame(Dates = dates, Dividends = dividends)
-
+write_xlsx(exportTable,"C:/Users/Sergio/Documents/Documents/Dividend/DividendTable.xlsx")
 
