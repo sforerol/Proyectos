fb2969a1b2e20264668773b9778f63fe21cc6fe6
sforerol <ser18fl@gmail.com>
2020-12-08 21:08:17 -0500

    Tested Next Step is to Fix Date and URL  Input

--- getDividends/script.R
+++ getDividends/script.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(rvest)
 library("writexl")
-url <- "https://finance.yahoo.com/quote/SPSM/history?period1=1448668800&period2=1606521600&interval=div%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true"
+url <- "https://finance.yahoo.com/quote/GE/history?period1=1448668800&period2=1606521600&interval=div%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true"
 h <- read_html(url)
 tab <- h %>% html_nodes("table")
 table <- tab %>% html_table(fill = TRUE)
 tabledf <- data.frame(table)
-tabledf <- tabledf[1:21,1:2]
+endrow <- nrow(tabledf)-1
+tabledf <- tabledf[1:endrow,1:2]
 dates <- tabledf[,1]
 dividends <-gsub(" .*$","",tabledf[,2])
 dividends <- as.numeric(dividends)
